b61a7a881e6802f7f2afadee53e703aa09760de8
Huexxx <huexxx@gmail.com>
2013-12-01 10:50:45 +0100

    Add status bar native battery percentage

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2429,6 +2429,12 @@ public final class Settings {
        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether to show/hide status bar native battery percentage
+         * @hide
+         */
+        public static final String STATUS_BAR_NATIVE_BATTERY_PERCENTAGE = "status_bar_native_battery_percentage";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
